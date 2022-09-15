cc9c17efb6bfa40801b36345f8c0810530cfa3b1
Pablo <juansiediaz97@gmail.com>
2022-09-15 13:40:29 -0700

    Testing

--- app/src/main/java/edu/csub/cs/barberrater/MainActivity.java
+++ app/src/main/java/edu/csub/cs/barberrater/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //this is my test comment
+        //This is a test comment from Juan
     }
 }
