38e2c08f6eacca01bb027e57a36b4bfae74486fb
jsierradiaz <89560629+jsierradiaz@users.noreply.github.com>
2023-04-24 19:42:37 -0700

    Update to AI screen. Display response from server.

--- flutter_baberrater/lib/screens/AI_screen.dart
+++ flutter_baberrater/lib/screens/AI_screen.dart
@@ -39,6 +39,10 @@ class AI_screen extends StatefulWidget {
 class _AI_screenState extends State<AI_screen> {
   // Selected Image storing in a Variable
   File? _selectedImage;
+  bool _uploaded = false;
+
+  var face_shape;
+  var image_file = '';
 
   Future<http.Response> uploadImage(File file, String link) async {
     ///MultiPart request
@@ -63,14 +67,13 @@ class _AI_screenState extends State<AI_screen> {
     print("This is response:" + response.body);
     print("This is response: ${res.statusCode} ");
     print("This is response: ${res.statusCode} ");
+
     return response;
   }
 
   Future<void> _addImage() async {
-    final image = await ImagePicker.platform.pickImage(
-          source: ImageSource.gallery,
-          imageQuality: 60,
-    );
+    final image =
+        await ImagePicker.platform.pickImage(source: ImageSource.gallery);
     if (image != null) {
       _selectedImage = File(image.path);
     }
@@ -100,14 +103,31 @@ class _AI_screenState extends State<AI_screen> {
                           width: MediaQuery.of(context).size.width * 0.8,
                           child: Image.file(File(_selectedImage!.path))),
                       TextButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.blue)
+                        ),
                           onPressed: () async {
                             final res = await uploadImage(
                                 File(_selectedImage!.path), "https://calculon.cs.csubak.edu/alonso/image/");
                             debugPrint(res.body);
-                            setState(() {});
+                            final val = jsonDecode(res.body);
+                            face_shape = val['face_shape'];
+                            image_file = val['file_name'];
+
+                            //print(face_shape);
+                            //print(path);
+                            setState(() {_uploaded = true;});
                           },
-                          child: const Text("Upload Image")),
-               
+                          child: const Text("Upload Image", style: TextStyle(color: Colors.white),)),
+                      if(_uploaded)
+                        Text(
+                          "Your faceshape is: " + face_shape +". Recommended hairstyles:",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.black, fontSize: 25),
+                        ),
+                      if(_uploaded)
+                        Image.asset("./assets/images/styles/male/" + image_file, height: 400, width: 400,)
+                      
                     ],
                   ),
               ]),
