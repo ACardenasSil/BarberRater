e71d6e7249dac1ba87e740a423b49370a237e7c3
edgar-rosales95 <95332608+edgar-rosales95@users.noreply.github.com>
2023-04-26 23:29:47 -0700

    fixed a posts and added maps

--- flutter_baberrater/.dart_tool/flutter_build/dart_plugin_registrant.dart
+++ flutter_baberrater/.dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -6,8 +6,10 @@
 // @dart = 2.18
 
 import 'dart:io'; // flutter_ignore: dart_io_import.
+import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
 import 'package:image_picker_android/image_picker_android.dart';
 import 'package:path_provider_android/path_provider_android.dart';
+import 'package:google_maps_flutter_ios/google_maps_flutter_ios.dart';
 import 'package:image_picker_ios/image_picker_ios.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
 import 'package:path_provider_linux/path_provider_linux.dart';
@@ -20,6 +22,16 @@ class _PluginRegistrant {
   @pragma('vm:entry-point')
   static void register() {
     if (Platform.isAndroid) {
+      try {
+        GoogleMapsFlutterAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`google_maps_flutter_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
       try {
         ImagePickerAndroid.registerWith();
       } catch (err) {
@@ -41,6 +53,16 @@ class _PluginRegistrant {
       }
 
     } else if (Platform.isIOS) {
+      try {
+        GoogleMapsFlutterIOS.registerWith();
+      } catch (err) {
+        print(
+          '`google_maps_flutter_ios` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
       try {
         ImagePickerIOS.registerWith();
       } catch (err) {
---
+++ flutter_baberrater/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/Objects-normal/arm64/path_provider_foundation_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/edgarrosales/Desktop/CSUB_COURSEWORK/Spring23/BarberRater/flutter_baberrater/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/DerivedSources/path_provider_foundation_vers.c
